3599833a1fd37d7d27ffee95fb9bc9e261361dff
tripurakavuri <30645227+tripurakavuri@users.noreply.github.com>
2017-11-22 16:16:06 +0530

    Add files via upload

---
+++ html/myscript.js
@@ -0,0 +1,6 @@
+var image1=new Image()
+image1.src="/webpages/gath4/general.jpg"
+var image2=new Image()
+image1.src="/webpages/gath3/image.jpg"
+var image1=new Image()
+image1.src="/webpages/gath5/intensive.jpg"
